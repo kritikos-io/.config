d4cf00f3f7a6b46ee6e4a5f06a8a570e4abb5d7e
Konstantinos Kokkinopoulos <kkokkino@gmail.com>
2021-08-17 17:22:36 +0300

    Rebranding for me myself and... me

--- scripts/install.ps1
+++
@@ -1,7 +0,0 @@
-dotnet tool update -g powershell
-git submodule update --init
-
-pwsh -File .config/scripts/windows/base.ps1
-## Any required environments
-pwsh -File .config/scripts/windows/dotnet.ps1
-Read-Host
--- scripts/windows/node.ps1
+++
@@ -1,5 +0,0 @@
-$links = $(Get-Item .).Parent.Parent
-$target = $links.Parent
-
-# ./base.ps1
-New-Item -Path $target\gulpfile.js -ItemType SymbolicLink -Value .config\gulpfile.js -Force
